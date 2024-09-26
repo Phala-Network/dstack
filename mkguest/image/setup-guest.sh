2fa55df26eeb5df1d82c67e046f099d7443ebf9b
Kevin Wang <wy721@qq.com>
2024-09-26 13:43:12 +0000

    Optimize makefile

--- mkguest/image/setup-guest.sh
+++ mkguest/image/setup-guest.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 set -e
+apt-get update
+apt-get install wireguard-tools -y
 curl -fsSL https://get.docker.com -o get-docker.sh
 sh get-docker.sh --version 27.1.2
 systemctl daemon-reload
