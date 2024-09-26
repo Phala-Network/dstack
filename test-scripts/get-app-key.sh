6cede02f9675a6f388c4bd15079a93b7a437a853
Kevin Wang <wy721@qq.com>
2024-09-26 07:29:51 +0000

    Remove certs dir

--- test-scripts/get-app-key.sh
+++ test-scripts/get-app-key.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 CERT_FILE=${1}
 KEY_FILE=${CERT_FILE%.*}.key
-URL=https://localhost:8000/prpc/KMS.GetAppKey
+CERT_DIR=../certs
+URL=https://localhost:8043/prpc/KMS.GetAppKey
 
 if [ -z "$CERT_FILE" ]; then
-    curl -s --cacert ca.cert ${URL}
+    curl -s --cacert ${CERT_DIR}/ca.cert ${URL}
 else
-    curl -s --cacert ca.cert --cert ${CERT_FILE} --key ${KEY_FILE} ${URL}
+    curl -vv --cacert ${CERT_DIR}/ca.cert --cert ${CERT_FILE} --key ${KEY_FILE} ${URL}
 fi
--- test-scripts/tproxy-reg.sh
+++ test-scripts/tproxy-reg.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 CERT_FILE=${1}
 KEY_FILE=${CERT_FILE%.*}.key
+CERT_DIR=../certs
 URL=https://localhost:8010/prpc/Tproxy.RegisterCvm?json
 
 D='{"wg_public_key": "4WyZldIHByffCulT674/n/ZLFH8jsfMZPkEnNOPaaW8="}'
 
 if [ -z "$CERT_FILE" ]; then
-    curl -s -d ${D} --cacert certs/ca.cert ${URL}
+    curl -s -d ${D} --cacert ${CERT_DIR}/ca.cert ${URL}
 else
-    curl -s -d "${D}" --cacert certs/ca.cert --cert ${CERT_FILE} --key ${KEY_FILE} ${URL}
+    curl -s -d "${D}" --cacert ${CERT_DIR}/ca.cert --cert ${CERT_FILE} --key ${KEY_FILE} ${URL}
 fi
