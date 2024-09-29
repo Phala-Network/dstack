971c3d669e2e9402aa0b1da72f209da6c5ec5686
Kevin Wang <wy721@qq.com>
2024-09-29 01:32:20 +0000

    Mod default build config values

--- build.sh
+++ build.sh
@@ -1,29 +1,40 @@
 #!/bin/bash
 
+SCRIPT_DIR=$(cd $(dirname $0); pwd)
+
+CONFIG_FILE=$SCRIPT_DIR/build-config.sh
+if [ -f $CONFIG_FILE ]; then
+    source $CONFIG_FILE
+else
+    cat <<EOF > $CONFIG_FILE
 # base domain of kms rpc and tproxy rpc
 # 1022.kvin.wang resolves to 10.0.2.2 which is host ip at the
 # cvm point of view
 BASE_DOMAIN=1022.kvin.wang
 
 # kms and tproxy rpc listen port
-TEEPOD_LISTEN_PORT=9080
+TEEPOD_RPC_LISTEN_PORT=9080
 KMS_RPC_LISTEN_PORT=9043
-
 TPROXY_RPC_LISTEN_PORT=9010
-TPROXY_WG_INTERFACE=tproxy0
-TPROXY_WG_LISTEN_PORT=51821
-TPROXY_WG_IP=10.0.4.1
-TPROXY_WG_CLIENT_IP_RANGE=10.0.4.0/24
 
-TPROXY_LISTEN_PORT1=9443  # The public port of tproxy
-TPROXY_LISTEN_PORT2=9090  # The public port of tproxy
+TPROXY_WG_INTERFACE=tproxy-$USER
+TPROXY_WG_LISTEN_PORT=9182
+TPROXY_WG_IP=10.0.3.1
+TPROXY_WG_CLIENT_IP_RANGE=10.0.3.0/24
 
-TPROXY_TARGET_PORT1=8080  # The target port of tproxy
-TPROXY_TARGET_PORT2=8090  # The target port of tproxy
+TPROXY_LISTEN_PORT1=9443
+TPROXY_LISTEN_PORT2=9090
+
+TPROXY_PUBLIC_DOMAIN=app.kvin.wang
+TPROXY_CERT=/etc/rproxy/certs/cert.pem
+TPROXY_KEY=/etc/rproxy/certs/key.pem
+EOF
+    echo "Config file $CONFIG_FILE created, please edit it to configure the build"
+    exit 1
+fi
 
 TPROXY_WG_KEY=$(wg genkey)
 TPROXY_WG_PUBKEY=$(echo $TPROXY_WG_KEY | wg pubkey)
-TPROXY_PUBLIC_DOMAIN=app.kvin.wang
 
 CERTS_DIR=`pwd`/certs
 IMAGES_DIR=`pwd`/images
@@ -96,13 +107,13 @@ interface = "$TPROXY_WG_INTERFACE"
 endpoint = "10.0.2.2:$TPROXY_WG_LISTEN_PORT"
 
 [core.proxy]
-cert_chain = "/etc/rproxy/certs/cert.pem"
-cert_key = "/etc/rproxy/certs/key.pem"
+cert_chain = "$TPROXY_CERT"
+cert_key = "$TPROXY_KEY"
 base_domain = "$TPROXY_PUBLIC_DOMAIN"
 config_path = "$RUN_DIR/rproxy.yaml"
 portmap = [
-    { listen_addr = "0.0.0.0", listen_port = $TPROXY_LISTEN_PORT1, target_port = $TPROXY_TARGET_PORT1 },
-    { listen_addr = "0.0.0.0", listen_port = $TPROXY_LISTEN_PORT2, target_port = $TPROXY_TARGET_PORT2 },
+    { listen_addr = "0.0.0.0", listen_port = $TPROXY_LISTEN_PORT1, target_port = 8080 },
+    { listen_addr = "0.0.0.0", listen_port = $TPROXY_LISTEN_PORT2, target_port = 8090 },
 ]
 EOF
 
@@ -110,7 +121,7 @@ EOF
 cat <<EOF > teepod.toml
 [default]
 log_level = "info"
-port = $TEEPOD_LISTEN_PORT
+port = $TEEPOD_RPC_LISTEN_PORT
 image_path = "$IMAGES_DIR"
 run_path = "$RUN_DIR/vm"
 
