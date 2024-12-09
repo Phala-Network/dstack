da6a14b4f491c65521a9a69382da8a93981028b1
Franco Barpp Gomes <franco@nethermind.io>
2024-12-09 19:55:55 +0530

    docs: Mention when altNames is included in the request

--- sdk/go/tappd/client.go
+++ sdk/go/tappd/client.go
@@ -194,7 +194,8 @@ func (c *TappdClient) sendRPCRequest(ctx context.Context, path string, payload i
 	return io.ReadAll(resp.Body)
 }
 
-// Derives a key from the Tappd service.
+// Derives a key from the Tappd service. If altNames is empty or nill, it will
+// not be included inthe request.
 func (c *TappdClient) DeriveKey(ctx context.Context, path string, subject string, altNames []string) (*DeriveKeyResponse, error) {
 	payload := map[string]interface{}{
 		"path":    path,
