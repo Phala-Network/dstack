3e9fd9b286bfbf9fe4a5de0931dd34ba51f9d2d5
Kevin Wang <wy721@qq.com>
2024-12-19 06:46:26 +0000

    guest: Don't run docker compose pull

--- basefiles/app-compose.sh
+++ basefiles/app-compose.sh
@@ -1,15 +1,7 @@
 #!/bin/sh
-
-tdxctl notify-host -e "boot.progress" -d "pulling images" || true
-if ! docker compose pull; then
-    tdxctl notify-host -e "boot.error" -d "failed to pull images"
-    exit 1
-fi
-
 tdxctl notify-host -e "boot.progress" -d "starting containers" || true
 if ! docker compose up -d; then
     tdxctl notify-host -e "boot.error" -d "failed to start containers"
     exit 1
 fi
-
 tdxctl notify-host -e "boot.progress" -d "done" || true
