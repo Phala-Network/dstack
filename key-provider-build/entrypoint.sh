ecb0a9c1487757ab88019bb37f0ff9ca6851a9ce
Kevin Wang <wy721@qq.com>
2025-01-09 14:11:05 +0000

    Print key provider MR

--- key-provider-build/entrypoint.sh
+++ key-provider-build/entrypoint.sh
@@ -2,5 +2,8 @@
 rsyslogd
 AESM_PATH=/opt/intel/sgx-aesm-service/aesm LD_LIBRARY_PATH=/opt/intel/sgx-aesm-service/aesm /opt/intel/sgx-aesm-service/aesm/aesm_service
 
+echo "Enclave info:"
+gramine-sgx-sigstruct-view --output-format json gramine-sealing-key-provider.sig
+
 echo "Starting Gramine Sealing Key Provider"
 make SGX=1 run-provider
