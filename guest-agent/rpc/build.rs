d0fb6853e8e561bb464cba7c9320cf435d5521d3
Kevin Wang <wy721@qq.com>
2025-04-02 03:36:03 +0000

    guest-agent: Fix service name emittion

--- guest-agent/rpc/build.rs
+++ guest-agent/rpc/build.rs
@@ -5,6 +5,9 @@ fn main() {
         .build_scale_ext(false)
         .disable_package_emission()
         .enable_serde_extension()
+        .disable_service_name_emission()
+        .keep_service_name("Tappd")
+        .keep_service_name("Worker")
         .compile_dir("./proto")
         .expect("failed to compile proto files");
 }
